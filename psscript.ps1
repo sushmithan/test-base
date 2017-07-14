7950190a0cc06cea14f44369930778ce343a6102
sushmithan <sushmithan@sysgain.com>
2017-07-14 16:19:06 +0530

    Update psscript.ps1

--- psscript.ps1
+++ psscript.ps1
@@ -1,3 +1,7 @@
+Invoke-WebRequest -Uri https://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SQLEXPRADV_x64_ENU.exe -OutFile D:\SQLEXPRADV_x64_ENU.exe
+
+D:\SQLEXPRADV_x64_ENU.exe /ACTION=install /QS /INSTANCENAME="mssqlserver" /SECURITYMODE=SQL /SA PWD="Sysgain@123456" /IACCEPTSQLSERVERLICENSETERMS=1 /FEATURES=SQLENGINE,SSMS /SQLSYSADMINACCOUNTS="adVM\windowsuser
+
 $domainName = "pizza.com"
 
 $username = "pizza.com\sysgain"
