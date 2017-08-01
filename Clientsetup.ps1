c468074b8b9f21132c5cc6090291740c50499835
sushmithan <sushmithan@sysgain.com>
2017-08-01 14:41:21 +0530

    Update Clientsetup.ps1

--- Clientsetup.ps1
+++ Clientsetup.ps1
@@ -2,4 +2,4 @@ param(
 [string] $splunkfqdn = "$1"
 )
 Invoke-WebRequest  -Uri https://download.splunk.com/products/universalforwarder/releases/6.6.2/windows/splunkforwarder-6.6.2-4b804538c686-x64-release.msi -OutFile c:/users/splunkforwarder-6.6.2-4b804538c686-x64-release.msi
-c:/users/splunkforwarder-6.6.2-4b804538c686-x64-release.msi AGREETOLICENSE=Yes RECEIVING_INDEXER=$splunkfqdn:9997 "C:\Windows\Logs" SERVICESTARTTYPE=auto /qn
+c:/users/splunkforwarder-6.6.2-4b804538c686-x64-release.msi AGREETOLICENSE=Yes RECEIVING_INDEXER="${splunkfqdn}:9997" MONITOR_PATH="C:\Windows\Logs" SERVICESTARTTYPE=auto /qn
